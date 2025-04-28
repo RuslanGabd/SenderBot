6218585d4792505999fe5d86a2d1864ebc536237
Ruslan G. <marlinco263@gmail.com>
2025-04-28 16:19:23 +0200

    added postgresSQL

--- src/main/java/telegram/bot/Main.java
+++ src/main/java/telegram/bot/Main.java
@@ -3,15 +3,11 @@ package telegram.bot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import telegram.bot.sendtomail.SenderBot;
-import telegram.bot.translator.TranslateBot;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
 
         botsApi.registerBot(new SenderBot());
-        botsApi.registerBot(new TranslateBot());
-
-
     }
 }
